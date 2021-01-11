bd5f84e289127ed9a81e468c6104b18eb1fcd01b
mianyan <565857351@qq.com>
2021-01-12 00:04:31 +0800

    加入dash播放器组件

---
+++ src/components/dash_player.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+  <video class="player" ref="videoPlayer" controls></video>
+</div>
+</template>
+
+<script>
+import dashjs from 'dashjs';
+
+export default {
+  name: "dash_player",
+  data() {
+    return {
+      // url: "https://s3.amazonaws.com/_bc_dml/example-content/sintel_dash/sintel_vod.mpd"
+      url: 'http://192.168.3.2:2050/dash/87199a6c-2ad5-4f75-9215-42016882ea72.mpd',
+      player: null
+    }
+  },
+  methods: {
+  },
+  mounted() {
+    this.player = dashjs.MediaPlayer().create();
+    this.player.initialize(this.$refs.videoPlayer, this.url, true);
+  },
+}
+</script>
+
+<style scoped>
+.player {
+  width: 640px;
+  height: 320px
+}
+</style>
