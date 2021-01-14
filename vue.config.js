321e5c20742f0ed9015b027620eef890ec14eb71
mianyan <565857351@qq.com>
2021-01-15 00:26:32 +0800

    解决跨域问题，对后端服务器设置前端代理

---
+++ vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    publicPath: '/',
+    devServer: {
+        host: '0.0.0.0',
+        port: '8080',
+        hot: true,
+        open: false,
+        overlay: {
+            warning: false,
+            error: true
+        },
+
+        //跨域代理
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8000',
+                changeOrigin: true,
+                ws: true,
+                pathRewrite: {
+                    '^/api/': '/'
+                }
+            },
+        },
+    },
+}
