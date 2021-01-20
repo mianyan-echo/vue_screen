eb10ca42f4351253280226651c573de01af2f161
mianyan <565857351@qq.com>
2021-01-21 00:03:18 +0800

    加入视频小窗最小化到底栏功能

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,51 @@
       <el-header>远程监控系统</el-header>
       <el-main>
         <img alt="Vue logo" src="./assets/logo.png">
-        <Dash_play_window v-for="ip_camera in ip_cameras"
-                          :key="ip_camera"
-                          :cam="ip_camera"/>
+        <Dash_play_window v-for="ip_camera in ip_cameras" :key="ip_camera"
+                          :cam="ip_camera" @onMinusValue="onMinusValue"/>
       </el-main>
+
+      <el-button @click="$refs.footer.open()">打开底部抽屉</el-button>
+      <footer_drawer ref="footer">
+        <dash_play_card v-for="minus of minus_set" :key="minus"
+                        :dash_url="minus" @CloseCard="onCloseCard"></dash_play_card>
+      </footer_drawer>
+
     </el-container>
   </div>
 </template>
 
 <script>
 import Dash_play_window from "@/components/dash_play_window";
+import footer_drawer from "@/components/footer_drawer";
+import dash_play_card from "@/components/dash_play_card";
+
 import {ref} from 'vue';
 
 export default {
   name: 'App',
   components: {
     Dash_play_window,
+    footer_drawer,
+    dash_play_card
   },
 
   setup() {
     const ip_cameras = ref(null);
 
+    const minus_set = ref(new Set());
+
     return {
       ip_cameras,
+      minus_set,
+    }
+  },
+  methods: {
+    onMinusValue(value) {
+      this.minus_set.add(value)
+    },
+    onCloseCard(value) {
+      this.minus_set.delete(value)
     }
   },
   mounted() {
