e228f7e1d35f814fd1bc04b62ae5b45ecbe8aad9
mianyan <565857351@qq.com>
2021-01-20 23:58:56 +0800

    底部弹出的抽屉组件

---
+++ src/components/footer_drawer.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="footer_drawer">
+  <el-drawer
+      v-model="is_show"
+      :modal="false"
+      direction="btt"
+      size=45%>
+
+    <el-scrollbar>
+      <slot>
+        <el-empty></el-empty>
+      </slot>
+    </el-scrollbar>
+  </el-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "footer_drawer",
+      data() {
+      return {
+        is_show: false
+      }
+    },
+    methods: {
+      close() {
+        this.is_show = false
+      },
+      open(){
+        this.is_show = true
+      }
+    }
+
+}
+</script>
+
+<style>
+.el-drawer__body {
+  overflow: auto;
+}
+</style>
