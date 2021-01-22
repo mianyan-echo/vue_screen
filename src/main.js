a652a193dbbab913f6e5678b454ea0d001ffcac3
mianyan <565857351@qq.com>
2021-01-22 15:01:34 +0800

    导入echarts5等可视化库

--- src/main.js
+++ src/main.js
@@ -6,12 +6,19 @@ import {axios, API} from "@/rest_api";
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
 
+//导入echarts和DataV
+import * as echarts from 'echarts';
+import 'echarts-gl'
+import dataV from '@jiaminghi/data-view'
+
 const app = createApp(App);
 
 app.use(ElementPlus);
+app.use(dataV);
 
 app.config.globalProperties.$axios = axios;
 app.config.globalProperties.$rest_api = API; // 将API接口包注册到Vue全局
+app.config.globalProperties.$echarts = echarts;
 
 // app.config.productionTip = false;
 app.mount('#app');
