415f0b77e5705943ff95414603e8c7ec2576c735
mianyan <565857351@qq.com>
2021-02-04 17:34:57 +0800

    剔除datav可视化组件

--- src/main.js
+++ src/main.js
@@ -9,12 +9,10 @@ import 'element-plus/lib/theme-chalk/index.css';
 //导入echarts和DataV
 import * as echarts from 'echarts';
 import 'echarts-gl'
-import dataV from '@jiaminghi/data-view'
 
 const app = createApp(App);
 
 app.use(ElementPlus);
-app.use(dataV);
 
 app.config.globalProperties.$axios = axios;
 app.config.globalProperties.$rest_api = API; // 将API接口包注册到Vue全局
